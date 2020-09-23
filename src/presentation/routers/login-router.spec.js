4e1a9440ffd27fb3cabf9e27072b39f39f946add
Anderson Carubelli <acarubelli@gmail.com>
2020-09-23 07:48:40 -0300

    ensure loginRouter returns 500 if an invalid httpRequest is provided
    Signed-off-by: Anderson Carubelli <acarubelli@gmail.com>

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,34 +1,51 @@
 class LoginRouter {
     route(httpRequest) {
-        const { email, password } = httpRequest.body
+        if (!httpRequest || !httpRequest.body) {
+            return {
+                statusCode: 500,
+            };
+        }
+        const { email, password } = httpRequest.body;
         if (!email || !password) {
             return {
-                statusCode: 400
-            }
+                statusCode: 400,
+            };
         }
     }
 }
 
-describe('Login Router', () => {
-    test('Should return 400 if no email is provided', () => {
-        const sut = new LoginRouter()
+describe("Login Router", () => {
+    test("Should return 400 if no email is provided", () => {
+        const sut = new LoginRouter();
         const httpRequest = {
             body: {
-                password: 'any_password'
-            }
-        }
-        const httpResponse = sut.route(httpRequest)
-        expect(httpResponse.statusCode).toBe(400)
-    })
+                password: "any_password",
+            },
+        };
+        const httpResponse = sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+    });
 
-    test('Should return 400 if no password is provided', () => {
-        const sut = new LoginRouter()
+    test("Should return 400 if no password is provided", () => {
+        const sut = new LoginRouter();
         const httpRequest = {
             body: {
-                email: 'any_email@mail.com'
-            }
-        }
-        const httpResponse = sut.route(httpRequest)
-        expect(httpResponse.statusCode).toBe(400)
-    })
-})
+                email: "any_email@mail.com",
+            },
+        };
+        const httpResponse = sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+    });
+
+    test("Should return 500 if no httpRequest is provided", () => {
+        const sut = new LoginRouter();
+        const httpResponse = sut.route();
+        expect(httpResponse.statusCode).toBe(500);
+    });
+
+    test("Should return 500 if no httpRequest has no body", () => {
+        const sut = new LoginRouter();
+        const httpResponse = sut.route({});
+        expect(httpResponse.statusCode).toBe(500);
+    });
+});
