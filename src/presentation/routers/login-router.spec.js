c06b0404ca17454a439916f226fd3c26b5afca1d
Anderson Carubelli <acarubelli@gmail.com>
2020-09-24 07:40:29 -0300

    ensure login router returns 401 when invalid credentials are provided
    Signed-off-by: Anderson Carubelli <acarubelli@gmail.com>

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -63,4 +63,17 @@ describe("Login Router", () => {
         expect(authUseCaseSpy.email).toBe(httpRequest.body.email);
         expect(authUseCaseSpy.password).toBe(httpRequest.body.password);
     });
+
+    test("Should return 401 when invalid credentials are provided", () => {
+        const { sut, authUseCaseSpy } = makeSut();
+        const httpRequest = {
+            body: {
+                email: "invalid_email@mail.com",
+                password: "invalid_password",
+            },
+        };
+
+        const httpResponse = sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(401);
+    });
 });
