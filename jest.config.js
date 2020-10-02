c37d78da4736ba3e05ec4649e53d5b66d175e196
Anderson Carubelli <acarubelli@gmail.com>
2020-10-02 07:46:24 -0300

    add new jest configs
    Signed-off-by: Anderson Carubelli <acarubelli@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     coverageDirectory: "coverage",
-    coverageProvider: "v8",
     testEnvironment: "node",
+    collectCoverageFrom: ["**/src/**/*.js", "!**/src/main/**"],
+    preset: "@shelf/jest-mongodb",
 };
