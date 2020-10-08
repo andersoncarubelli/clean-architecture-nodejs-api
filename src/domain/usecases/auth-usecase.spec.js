b442c00e48448e9c67e06ddbf8d1054eb5d3b7d4
Anderson Carubelli <acarubelli@gmail.com>
2020-10-08 07:03:52 -0300

    ensure AuthUseCase throws if any dependency throws
    Signed-off-by: Anderson Carubelli <acarubelli@gmail.com>

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -81,6 +81,16 @@ const makeUpdateAccessTokenRepository = () => {
     return new UpdateAccessTokenRepositorySpy();
 };
 
+const makeUpdateAccessTokenRepositoryWithError = () => {
+    class UpdateAccessTokenRepositorySpy {
+        async update() {
+            throw new Error();
+        }
+    }
+
+    return new UpdateAccessTokenRepositorySpy();
+};
+
 const makeSut = () => {
     const encrypterSpy = makeEncrypter();
     const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository();
@@ -249,6 +259,7 @@ describe("Auth UseCase", () => {
     test("Should throw if any dependency throws", async () => {
         const loadUserByEmailRepository = makeLoadUserByEmailRepository();
         const encrypter = makeEncrypter();
+        const tokenGenerator = makeTokenGenerator();
 
         const suts = [].concat(
             new AuthUseCase({
@@ -262,6 +273,12 @@ describe("Auth UseCase", () => {
                 loadUserByEmailRepository,
                 encrypter,
                 tokenGenerator: makeTokenGeneratorWithError(),
+            }),
+            new AuthUseCase({
+                loadUserByEmailRepository,
+                encrypter,
+                tokenGenerator,
+                updateAccessTokenRepository: makeUpdateAccessTokenRepositoryWithError(),
             })
         );
 
