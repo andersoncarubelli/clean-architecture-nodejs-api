37554bacb19c74f673db2efed8b5b63363e629e7
Anderson Carubelli <acarubelli@gmail.com>
2020-10-16 07:37:09 -0300

    ensure LoadUserByEmailRepository throws if no email is provided
    Signed-off-by: Anderson Carubelli <acarubelli@gmail.com>

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,4 +1,5 @@
 const LoadUserByEmailRepository = require("./load-user-by-email-repository");
+const MissingParamError = require("../../utils/errors/missing-param-error");
 const MongoHelper = require("../helpers/mongo-helper");
 
 let db;
@@ -48,5 +49,11 @@ describe("LoadUserByEmail Repository", () => {
         const sut = new LoadUserByEmailRepository();
         const promise = sut.load("any_email@mail.com");
         expect(promise).rejects.toThrow();
+    });
+
+    test("Should throw if no email is provided", async () => {
+        const { sut } = makeSut();
+        const promise = sut.load();
+        expect(promise).rejects.toThrow(new MissingParamError("email"));
     });
 });
