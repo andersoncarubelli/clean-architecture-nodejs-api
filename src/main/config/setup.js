6e2b81e7c0b004abbdb252f785b1f4882d8dcb19
Anderson Carubelli <acarubelli@gmail.com>
2020-10-17 13:01:51 -0300

    ensure requests parse body as JSON
    Signed-off-by: Anderson Carubelli <acarubelli@gmail.com>

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,6 +1,9 @@
 const cors = require("../middlewares/cors");
+const express = require("express");
+const jsonParser = require("../middlewares/json-parser");
 
 module.exports = (app) => {
     app.disable("x-powered-by");
     app.use(cors);
+    app.use(jsonParser);
 };
--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -7,7 +7,7 @@ describe("App Setup", () => {
             res.send("");
         });
 
-        const res = await request(app).get("/");
+        const res = await request(app).get("/test_x_powered_by");
         expect(res.headers["x-powered-by"]).toBeUndefined();
     });
 });
